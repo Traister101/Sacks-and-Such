4530f1beafba4fa5e4a76339be587bacc1a26e8d
Traister101 <66151907+Traister101@users.noreply.github.com>
2022-10-07 03:00:33 -0700

    Gave up thrown entity uses arrow now

--- src/main/java/mod/traister101/sacks/objects/entity/projectile/EntityExplosiveVessel.java
+++ src/main/java/mod/traister101/sacks/objects/entity/projectile/EntityExplosiveVessel.java
@@ -1,73 +1,134 @@
 package mod.traister101.sacks.objects.entity.projectile;
 
+import javax.annotation.Nonnull;
+
 import io.netty.buffer.ByteBuf;
+import mod.traister101.sacks.util.VesselType;
 import net.dries007.tfc.util.PowderKegExplosion;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
 
-public class EntityExplosiveVessel extends EntityThrowable implements IEntityAdditionalSpawnData {
+public class EntityExplosiveVessel extends EntityArrow implements IEntityAdditionalSpawnData {
 	
-	private final float strength;
+    private VesselType type;
+	
+	private float strength;
+	private boolean isSticky;
+	private int fuse;
 	
 	public EntityExplosiveVessel(World worldIn) {
 		super(worldIn);
-		this.strength = 5;
-	}
-	
-	public EntityExplosiveVessel(World worldIn, EntityLivingBase throwerIn, float strength) {
-		super(worldIn, throwerIn);
-		this.strength = strength;
+		setSize(0.5F, 0.5F);
+		this.setDamage(0);
 	}
 	
 	public EntityExplosiveVessel(World worldIn, double x, double y, double z) {
-		super(worldIn, x, y, z);
-		this.strength = 5;
+		this(worldIn);
+		setPosition(x, y, z);
 	}
 	
-	protected void explode() {
-		if (getEntityWorld().isRemote) return;
+	public EntityExplosiveVessel(World worldIn, EntityLivingBase throwerIn, float strength, @Nonnull VesselType type) {
+		this(worldIn, throwerIn.posX, throwerIn.posY + (double) throwerIn.getEyeHeight() - 0.10000000149011612D, throwerIn.posZ);
+		this.shootingEntity = throwerIn;
+		this.strength = strength;
+		this.type = type;
+		if (type == VesselType.STICKY) {
+			this.isSticky = true;
+		} else this.isSticky = false;
 		
-		PowderKegExplosion explosion = new PowderKegExplosion(getEntityWorld(), this, posX, posY, posZ, strength);
-		if (ForgeEventFactory.onExplosionStart(world, explosion)) return;
-		explosion.doExplosionA();
-		explosion.doExplosionB(true);
+		if (isSticky) fuse = 80;
 	}
 	
-	protected void onImpact(RayTraceResult result) {
+	@Override
+	protected void onHit(RayTraceResult result) {
+		
+		if (result.typeOfHit == RayTraceResult.Type.BLOCK && isSticky) {
+			motionX = (double) ((float) (result.hitVec.x - posX));
+			motionY = (double) ((float) (result.hitVec.y - posY));
+			motionZ = (double) ((float) (result.hitVec.z - posZ));
+			float f2 = MathHelper.sqrt(motionX * motionX + motionY * motionY + motionZ * motionZ);
+			posX -= motionX / (double) f2 * 0.05000000074505806D;
+			posY -= motionY / (double) f2 * 0.05000000074505806D;
+			posZ -= motionZ / (double) f2 * 0.05000000074505806D;
+			playSound(SoundEvents.ENTITY_SLIME_SQUISH, 1.0F, 1.2F / (rand.nextFloat() * 0.2F + 0.9F));
+			inGround = true;
+			
+			return;
+		}
 		
 		if (world.isRemote) return;
-		
-			
 		world.setEntityState(this, (byte) 3);
 		explode();
-		setDead();
+	}
+	
+	@Override
+	public void onUpdate() {
+		
+		if (inGround) {
+			
+			--fuse;
+			
+			if (fuse <= 0) if (!world.isRemote) explode();
+		}
+		
+		
+		super.onUpdate();
 	}
 	
 	@Override
 	public void writeEntityToNBT(NBTTagCompound compound) {
 		super.writeEntityToNBT(compound);
+		compound.setInteger("fuse", fuse);
+		compound.setBoolean("sticky", isSticky);
+		compound.setInteger("type", type.ordinal());
+		compound.setFloat("strength", strength);
 	}
 	
 	@Override
 	public void readEntityFromNBT(NBTTagCompound compound) {
 		super.readEntityFromNBT(compound);
+		fuse = compound.getInteger("fuse");
+		isSticky = compound.getBoolean("sticky");
+		type = VesselType.valueOf(compound.getInteger("type"));
+		strength = compound.getFloat("strength");
 	}
 	
 	@Override
 	public void writeSpawnData(ByteBuf buffer) {
-		
+		buffer.writeBoolean(isSticky);
+		buffer.writeInt(fuse);
+		buffer.writeInt(type.ordinal());
 	}
 	
 	@Override
-	public void readSpawnData(ByteBuf additionalData) {
-		
+	public void readSpawnData(ByteBuf buffer) {
+		isSticky = buffer.readBoolean();
+		fuse = buffer.readInt();
+		type = VesselType.valueOf(buffer.readInt());
+	}
+	
+	protected void explode() {
+		PowderKegExplosion explosion = new PowderKegExplosion(world, this, posX, posY, posZ, strength);
+		if (ForgeEventFactory.onExplosionStart(world, explosion)) return;
+		explosion.doExplosionA();
+		explosion.doExplosionB(true);
+		setDead();
+	}
+	
+	public VesselType getType() {
+		return type;
+	}
+
+	@Override
+	protected ItemStack getArrowStack() {
+		return null;
 	}
 }
