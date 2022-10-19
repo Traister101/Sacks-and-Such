c75d56273118b6c547baf75741a7fee91f832831
Traister101 <66151907+Traister101@users.noreply.github.com>
2022-10-19 05:36:31 -0700

    Fix Farmer sack in SackType

--- src/main/java/mod/traister101/sacks/util/SackType.java
+++ src/main/java/mod/traister101/sacks/util/SackType.java
@@ -10,7 +10,7 @@ public enum SackType {
     LEATHER(4, LEATHER_SACK.slotCap, LEATHER_SACK.doPickup, LEATHER_SACK.doVoiding, GuiType.SACK_LEATHER, LEATHER_SACK.allowedSize),
     BURLAP(4, BURLAP_SACK.slotCap, BURLAP_SACK.doPickup, BURLAP_SACK.doVoiding, GuiType.SACK_BURLAP, BURLAP_SACK.allowedSize),
     MINER(1, MINER_SACK.slotCap, MINER_SACK.doPickup, MINER_SACK.doVoiding, GuiType.SACK_MINER, MINER_SACK.allowedSize),
-    FARMER(27, FARMER_SACK.slotCap, FARMER_SACK.doPickup, FARMER_SACK.doVoiding, GuiType.SACK_FARMER, Size.NORMAL),
+    FARMER(27, FARMER_SACK.slotCap, FARMER_SACK.doPickup, FARMER_SACK.doVoiding, GuiType.SACK_FARMER, FARMER_SACK.allowedSize),
     KNAPSACK(18, KNAP_SACK.slotCap, KNAP_SACK.doPickup, KNAP_SACK.doVoiding, GuiType.SACK_KNAP, KNAP_SACK.allowedSize);
 
     public final int slots;
