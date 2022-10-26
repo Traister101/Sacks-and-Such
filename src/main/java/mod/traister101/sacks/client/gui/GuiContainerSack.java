f6632d756d44e3724d14812f9f8558f06611045d
Traister101 <66151907+Traister101@users.noreply.github.com>
2022-10-26 07:20:24 -0700

    Stripping down of slot drawing (unnecessary GLStateManager calls)

--- src/main/java/mod/traister101/sacks/client/gui/GuiContainerSack.java
+++ src/main/java/mod/traister101/sacks/client/gui/GuiContainerSack.java
@@ -3,11 +3,10 @@ package mod.traister101.sacks.client.gui;
 import mod.traister101.sacks.SacksNSuch;
 import mod.traister101.sacks.client.button.GuiVoidButton;
 import mod.traister101.sacks.network.TogglePacket;
-import mod.traister101.sacks.objects.inventory.slot.AbstractSlot;
+import mod.traister101.sacks.objects.inventory.slot.SackSlot;
 import mod.traister101.sacks.objects.items.ItemSack;
 import mod.traister101.sacks.util.SNSUtils.ToggleType;
 import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
@@ -54,14 +53,15 @@ public class GuiContainerSack extends GuiRenameable {
 
     private void drawSlots() {
         mc.getTextureManager().bindTexture(new ResourceLocation(SacksNSuch.MODID, "textures/gui/slot.png"));
-        GlStateManager.color(1, 1, 1, 1);
-        GlStateManager.disableLighting();
-        GlStateManager.disableRescaleNormal();
-        final int width = 18;
-        final int height = 18;
+        final int size = 18;
+        // The largest container we ship only renders 27 slots. That commeneded out loop makes it render
+        // 27,000 and only cuts the framerate in half I don't think performance is a valid concern
+//        for (int i = 0; i < 1000; i++)
         for (Slot slot : inventorySlots.inventorySlots) {
-            if (slot instanceof AbstractSlot) {
-                drawModalRectWithCustomSizedTexture(guiLeft + slot.xPos - 1, guiTop + slot.yPos - 1, width, height, width,  height, width, height);
+            if (slot instanceof SackSlot) {
+                final int x = guiLeft + slot.xPos - 1;
+                final int y = guiTop + slot.yPos - 1;
+                drawModalRectWithCustomSizedTexture(x, y, size, size, size, size, size, size);
             }
         }
     }
