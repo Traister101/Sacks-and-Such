668c6d6fc0fc7b0ddb8c0f328b20d2c94c8009c6
Traister101 <66151907+Traister101@users.noreply.github.com>
2022-11-02 19:50:42 -0700

    Fancy build file stuff

--- src/main/java/mod/traister101/sacks/SacksNSuch.java
+++ src/main/java/mod/traister101/sacks/SacksNSuch.java
@@ -24,13 +24,12 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = SacksNSuch.MODID, name = SacksNSuch.NAME, version = SacksNSuch.VERSION, dependencies = "required-after:tfc@1.7.18.176", useMetadata = true)
 public final class SacksNSuch {
 
-    public static final String MODID = "sns";
-    public static final String NAME = "Sacks 'N Such";
+    public static final String MODID = "@MODID@";
+    public static final String NAME = "@MODNAME@";
     public static final String VERSION = "@VERSION@";
 
     @Instance
     private static SacksNSuch INSTANCE = null;
-
     private final Logger log = LogManager.getLogger(MODID);
     private SimpleNetworkWrapper network;
 
